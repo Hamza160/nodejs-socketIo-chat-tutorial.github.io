023a7ba25b48e525cb7d8649204cda7c8ab2511a
Hamza Ashraf <hamzaashraf160@gmail.com>
2024-01-12 03:42:50 +0500

    Socket Connection Setup Successfully

--- src/server.js
+++ src/server.js
@@ -1,12 +1,27 @@
 const path = require('path');
 const express = require('express');
-
+const socketIo = require('socket.io');
+const http = require('http');
 const publicPath = path.join(__dirname, '/../public');
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => console.log(`Server is started on PORT:${PORT}`));
+const server = http.createServer(app);
+const io = socketIo(server);
+
+/*Listening The Event => When Client Connect To Server*/
+io.on('connection', (socket) => {
+    console.log(`A New User Just Connected`);
+
+    socket.on('disconnect', () => {
+        console.log(`User was disconnected`)
+    })
+});
+
+
+
+server.listen(PORT, () => console.log(`Server is started on PORT:${PORT}`));
 
 // console.log(__dirname + "/../public");
 // console.log(path.join(__dirname, '/../public'));
